0f6d5dec5ee2ac30111bec4325da9f0413bbcb50
sk-io <svensamkarl@gmail.com>
2023-11-28 06:59:53 +0100

    Add basic keyboard controls to DOOM

--- userspace/doomgeneric/doomgeneric/doomgeneric_os.c
+++ userspace/doomgeneric/doomgeneric/doomgeneric_os.c
@@ -1,99 +1,110 @@
-//doomgeneric for cross-platform development library 'Simple DirectMedia Layer'
+// doomgeneric for cross-platform development library 'Simple DirectMedia Layer'
 
+#include "doomgeneric.h"
 #include "doomkeys.h"
 #include "m_argv.h"
-#include "doomgeneric.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-#include <stdbool.h>
 #include <os.h>
+#include <stdbool.h>
 
 int window;
-int* fb;
+int *fb;
 
-void DG_Init(){
+#define KEYQUEUE_SIZE 16
+
+static unsigned short s_KeyQueue[KEYQUEUE_SIZE];
+static unsigned int s_KeyQueueWriteIndex = 0;
+static unsigned int s_KeyQueueReadIndex = 0;
+
+void DG_Init() {
     window = os_create_window(DOOMGENERIC_RESX, DOOMGENERIC_RESY, 0);
     fb = os_map_window_framebuffer(window);
-//   window = SDL_CreateWindow("DOOM",
-//                             SDL_WINDOWPOS_UNDEFINED,
-//                             SDL_WINDOWPOS_UNDEFINED,
-//                             DOOMGENERIC_RESX,
-//                             DOOMGENERIC_RESY,
-//                             SDL_WINDOW_SHOWN
-//                             );
-
-//   // Setup renderer
-//   renderer =  SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED);
-//   // Clear winow
-//   SDL_RenderClear( renderer );
-//   // Render the rect to the screen
-//   SDL_RenderPresent(renderer);
-
-//   texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_TARGET, DOOMGENERIC_RESX, DOOMGENERIC_RESY);
 }
 
-void DG_DrawFrame()
-{
-    // fb[
+static unsigned char ascii_to_doomkey(unsigned int key) {
+    switch (key) {
+    case 'w': return KEY_UPARROW;
+    case 's': return KEY_DOWNARROW;
+    case 'a': return KEY_STRAFE_L;
+    case 'd': return KEY_STRAFE_R;
 
+    case 'k': return KEY_LEFTARROW;
+    case 'l': return KEY_RIGHTARROW;
+    case 'u': return KEY_FIRE;
+
+    case 'o':
+    case 0x1b:
+        return KEY_ESCAPE;
+    case 'p':
+        return KEY_ENTER;
+    }
+    return -1;
+}
+
+static void add_key_to_queue(int pressed, unsigned int keyCode) {
+    unsigned char key = ascii_to_doomkey(keyCode);
+
+    unsigned short keyData = (pressed << 8) | key;
+
+    s_KeyQueue[s_KeyQueueWriteIndex] = keyData;
+    s_KeyQueueWriteIndex++;
+    s_KeyQueueWriteIndex %= KEYQUEUE_SIZE;
+}
+
+void DG_DrawFrame() {
     // for (int i = 0; i < DOOMGENERIC_RESX * DOOMGENERIC_RESY; i++)
     //     fb[i] = 0xFF00FF00;
     memcpy(fb, DG_ScreenBuffer, DOOMGENERIC_RESX * DOOMGENERIC_RESY * 4);
     for (int i = 0; i < DOOMGENERIC_RESX * DOOMGENERIC_RESY; i++)
         fb[i] |= 0xFF000000;
-    
+
     os_swap_window_buffers(window);
 
     OSEvent event;
     while (os_poll_event(&event)) {
+        if (event.type == OS_EVENT_KEYBOARD) {
+            OSKeyboardEvent *key_event = (OSKeyboardEvent *) &event;
+            add_key_to_queue(key_event->state == 1, key_event->ascii);
+        }
     }
 }
 
-void DG_SleepMs(uint32_t ms)
-{
-//   SDL_Delay(ms);
+void DG_SleepMs(uint32_t ms) {
+    // todo
 }
 
-uint32_t DG_GetTicksMs()
-{
-//   return SDL_GetTicks();
+uint32_t DG_GetTicksMs() {
     return os_get_system_time();
 }
 
-int DG_GetKey(int* pressed, unsigned char* doomKey)
-{
-//   if (s_KeyQueueReadIndex == s_KeyQueueWriteIndex){
-//     //key queue is empty
-//     return 0;
-//   }else{
-//     unsigned short keyData = s_KeyQueue[s_KeyQueueReadIndex];
-//     s_KeyQueueReadIndex++;
-//     s_KeyQueueReadIndex %= KEYQUEUE_SIZE;
+int DG_GetKey(int *pressed, unsigned char *doomKey) {
+    if (s_KeyQueueReadIndex == s_KeyQueueWriteIndex) {
+        // key queue is empty
+        return 0;
+    }
 
-//     *pressed = keyData >> 8;
-//     *doomKey = keyData & 0xFF;
+    unsigned short keyData = s_KeyQueue[s_KeyQueueReadIndex];
+    s_KeyQueueReadIndex++;
+    s_KeyQueueReadIndex %= KEYQUEUE_SIZE;
 
-//     return 1;
-//   }
+    *pressed = keyData >> 8;
+    *doomKey = keyData & 0xFF;
 
-  return 0;
+    return 1;
 }
 
-void DG_SetWindowTitle(const char * title)
-{
-//   if (window != NULL){
-//     SDL_SetWindowTitle(window, title);
-//   }
+void DG_SetWindowTitle(const char *title) {
+    os_set_window_title(window, title);
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     int num_doom_args = 3;
-    char* doom_args[] = {
+    char *doom_args[] = {
         "doom.exe",
         "-iwad",
         "DOOM1.WAD",
@@ -101,8 +112,7 @@ int main(int argc, char **argv)
 
     doomgeneric_Create(num_doom_args, doom_args);
 
-    for (int i = 0; ; i++)
-    {
+    for (int i = 0;; i++) {
         doomgeneric_Tick();
     }
 
